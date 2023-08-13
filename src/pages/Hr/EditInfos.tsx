b915f31b6c1789365cd67a188efaae45a8c66d7b
hakantulgac <108068761+hakantulgac@users.noreply.github.com>
2023-08-13 19:13:53 +0300

    Added intern details for hr and intern users. Improved, data types for db table columns.

--- src/pages/Hr/EditInfos.tsx
+++ src/pages/Hr/EditInfos.tsx
@@ -7,7 +7,6 @@ const EditInfos: React.FC = () => {
   const [editArea, setEditArea] = useState(true);
   const [username, setUsername] = useState(true);
   const [password, setPassword] = useState(true);
-  const [info,setInfo] = useState()
 
   const passwordValidator = (_: any, value: string) => {
     if (value !== "" && value.length < 5) {
